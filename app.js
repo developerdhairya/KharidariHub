dbd3bdc9c20629c2dd2d57493aeb66c63394bdf0
developerdhairya <dhairya0192.be20@chitkara.edu.in>
2022-06-05 03:22:37 +0530

    add logging

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 const express=require('express');
 const dotenv=require('dotenv');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const requestId = require('express-request-id');
 
 //initializing express
 const app=express();
@@ -13,5 +16,33 @@ dotenv.config();
 mongoose.connect(process.env.MONGO_URL,()=>{
     console.log("Connected to mongodb");
 });
+
+//Adding middlewares
+app.use(express.json());
+app.use(helmet());
+app.use(requestId());
+
+//setup logging cofigurations
+morgan.token("id",(req)=>{
+    return req.id;
+});
+const loggerFormat = ':id [:date[web]]" :method :url" :status :responsetime';
+app.use(morgan(loggerFormat,{
+    stream:process.stdout,
+    skip:(req,res)=>{
+        return res.statusCode>=400;
+    }
+}));
+app.use(morgan(loggerFormat,{
+    stream:process.stderr,
+    skip:(req,res)=>{
+        return res.statusCode<400;
+    }
+}));
+
+
+
+
+
 
 
