ef22e4ca11a4d7a22219221480d4fcaa49664256
developerdhairya <dhairya0192.be20@chitkara.edu.in>
2022-06-05 04:15:01 +0530

    setup mongodb connection

--- app.js
+++ app.js
@@ -1,32 +1,39 @@
-//importing modules
 const express=require('express');
 const dotenv=require('dotenv');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const helmet = require('helmet');
-const requestId = require('express-request-id');
+const uuid = require('uuid');
 
 //initializing express
 const app=express();
 
-//Loading contents from .env file into process
+//Loading contents from .env file
 dotenv.config();
 
-//Connecting to mongodb cluster
+
+//Assigning id to each request
+function assignId (req, res, next) {
+    req.id = uuid.v4()
+    next()
+  }
+
+// Connecting to mongodb cluster
 mongoose.connect(process.env.MONGO_URL,()=>{
-    console.log("Connected to mongodb");
+    console.log("MongoDB connection established successfully");
 });
 
 //Adding middlewares
 app.use(express.json());
 app.use(helmet());
-app.use(requestId());
+app.use(assignId);
+
 
 //setup logging cofigurations
 morgan.token("id",(req)=>{
     return req.id;
 });
-const loggerFormat = ':id [:date[web]]" :method :url" :status :responsetime';
+const loggerFormat = ':id [:date[web]]" :method :url" :status :response-time';
 app.use(morgan(loggerFormat,{
     stream:process.stdout,
     skip:(req,res)=>{
@@ -41,8 +48,10 @@ app.use(morgan(loggerFormat,{
 }));
 
 
+//routes
+app.get('/',async (req,res)=>{
+    return res.json({"hi":"hii"});
+});
 
 
-
-
-
+app.listen(8800, () => console.log("Backend Server is Running"));
